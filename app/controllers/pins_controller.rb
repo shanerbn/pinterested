79c4c0f78af0ec0566f29304a2f8dd4f4d5449e5
Shane Robinson <shane@converted.co.uk>
2015-02-09 23:54:10 +0000

    associating pins with users

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
 
   respond_to :html
 
@@ -43,5 +45,10 @@ class PinsController < ApplicationController
 
     def pin_params
       params.require(:pin).permit(:description)
+    end
+  
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 end
